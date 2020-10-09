69aca8d4da58032815e43d8cb47aa6c202a8b3e4
healthguru2020 <71390431+healthguru2020@users.noreply.github.com>
2020-10-09 18:45:28 +0200

    aggiunta vacuum lp e tolto integrazione sbagliata TA

---
+++ vacuum-lp/tarp_57810396551e070f7d96110e9411f011/index.php
@@ -0,0 +1,20 @@
+<?php
+$GLOBALS['_ta_rp_key'] = 'afdf915d89864e132cd9805f76e6b946';
+$GLOBALS['_ta_reverse_proxy_id'] = '7f69ad';
+
+require 'bootloader_57810396551e070f7d96110e9411f011.php';
+
+$options = array();
+/*
+$options = array(
+	'replace' => array(
+		'This is the text to find' => 'The new text',
+		'This is the text to find 2' => 'The new text 2',
+	)
+);
+*/
+
+$tarp = new TARPLoader($options);
+
+$tarp->excute();
+?>
